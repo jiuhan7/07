8259911f805b5bd62bd75f2febe357c2bc9bf248
jiuhan7 <144991445+jiuhan7@users.noreply.github.com>
2023-10-19 15:45:01 +0900

    3

---
+++ 3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int inc(int counter); 
+int main(void) 
+{
+	int i = 10;
+	printf("함수 호출전 i=%d\n", i);
+	i = inc(i);
+	printf("함수 호출후  i=%d\n", i);
+
+	return 0;
+}
+int inc(int counter)
+{
+	counter++;
+	return counter;
+}
